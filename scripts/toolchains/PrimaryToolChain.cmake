82d5d8226a88e9b6b22f7b67b3cadcc76c91a1f4
Sam Taseff <sgttaseff1@sheffield.ac.uk>
2023-12-05 03:44:10 +0000

    Update PrimaryToolChain.cmake

--- scripts/toolchains/PrimaryToolChain.cmake
+++ scripts/toolchains/PrimaryToolChain.cmake
@@ -2,4 +2,4 @@ set(VCPKG_TARGET_TRIPLET x64-windows-cyanseraph-app)
 
 
 # THIS MUST BE AT THE BOTTOM
-include($ENV{VCPKG}/scripts/buildsystems/vcpkg.cmake)
+include(${PROJECT_SOURCE_DIR}/vcpkg/scripts/buildsystems/vcpkg.cmake)
